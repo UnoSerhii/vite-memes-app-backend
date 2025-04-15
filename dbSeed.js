5ee2ce764dc432c04609c5421cc2b492af2c74b7
UnoSerhii <you@example.com>
2025-04-15 17:46:42 +0300

    added server

---
+++ dbSeed.js
@@ -0,0 +1,75 @@
+export const seed = (Mem) => {
+    function generateRandomLikes() {
+        return Math.floor(Math.random() * 100);
+    }
+
+
+    const initialMemes = [
+        {
+            id: uuidv4(),
+            name: 'Distracted Boyfriend',
+            image: 'https://i.imgflip.com/1ur9b0.jpg',
+            likes: generateRandomLikes(),
+        },
+        {
+            id: uuidv4(),
+            name: 'Drake Hotline Bling',
+            image: 'https://i.imgflip.com/30b1gx.jpg',
+            likes: generateRandomLikes(),
+        },
+        {
+            id: uuidv4(),
+            name: 'Roll Safe Think About It',
+            image: 'https://i.imgflip.com/1h7in3.jpg',
+            likes: generateRandomLikes(),
+        },
+        {
+            id: uuidv4(),
+            name: 'Is This a Pigeon?',
+            image: 'https://i.imgflip.com/1g8my4.jpg',
+            likes: generateRandomLikes(),
+        },
+        {
+            id: uuidv4(),
+            name: 'One Does Not Simply',
+            image: 'https://i.imgflip.com/1bij.jpg',
+            likes: generateRandomLikes(),
+        },
+        {
+            id: uuidv4(),
+            name: 'Success Kid',
+            image: 'https://i.imgflip.com/1bhk.jpg',
+            likes: generateRandomLikes(),
+        },
+        {
+            id: uuidv4(),
+            name: 'Grumpy Cat',
+            image: 'https://i.imgflip.com/8p0a.jpg',
+            likes: generateRandomLikes(),
+        },
+        {
+            id: uuidv4(),
+            name: 'Leonardo DiCaprio Cheers',
+            image: 'https://i.imgflip.com/39t1o.jpg',
+            likes: generateRandomLikes(),
+        },
+        {
+            id: uuidv4(),
+            name: 'Hide the Pain Harold',
+            image: 'https://i.imgflip.com/gk5el.jpg',
+            likes: generateRandomLikes(),
+        },
+        {
+            id: uuidv4(),
+            name: 'Waiting Skeleton',
+            image: 'https://i.imgflip.com/265k.jpg',
+            likes: generateRandomLikes(),
+        },
+    ];
+
+    initialMemes.forEach(async meme => {
+        await Mem.create({
+            ...meme
+        })
+    })
+}
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const {v4: uuidv4} = require('uuid');
+const {Sequelize, Model, DataTypes} = require('sequelize');
+const {seed} = require("./dbSeed");
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+});
+
+class Mem extends Model {
+}
+
+Mem.init({
+    id: {
+        type: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+    likes: DataTypes.NUMBER,
+}, {sequelize, modelName: 'mem'});
+
+sequelize.sync();
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/memes', async (req, res) => {
+    const memes = await Mem.findAll();
+    res.json(memes);
+});
+
+app.patch('/api/memes/:id', async (req, res) => {
+    const mem = await Mem.findOne({where: {id: req.params.id}});
+    if (!mem) return null;
+
+    const updatedMem = await mem.update(req.body, {returning: true});
+    res.json(updatedMem);
+})
+
+seed(Mem)
+
+app.listen(port, () => {
+    console.log(`Сервер запущено на порту ${port}`);
+});
